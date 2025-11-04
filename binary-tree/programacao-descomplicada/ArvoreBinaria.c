a5e09515f560976b7d54aea87f165c659cb2cf26
marcelloCoder <deoliveiral767@gmail.com>
2025-11-04 20:27:34 -0300

    Binarytree

--- binary-tree/programacao-descomplicada/ArvoreBinaria.c
+++ binary-tree/programacao-descomplicada/ArvoreBinaria.c
@@ -16,8 +16,71 @@ ArvoreBinaria* criaArvoreBinaria() {
     return raiz;
 }
 
+void liberaNO(struct NO* no) {
+    if(no == NULL){
+        return;
+    }
+    liberaNO(no->esq);
+    liberaNO(no->dir);  
+    free(no);
+    no = NULL;
+}
+
+void liberaArvoreBinaria(ArvoreBinaria* raiz) {
+    if(raiz == NULL){
+        return;
+    }
+    liberaArvoreBinaria(*raiz); 
+    free(raiz);
+}
+
+int estaVaziaArvoreBinaria(ArvoreBinaria* raiz) {
+    if(raiz == NULL){
+        return 1;
+    }
+    if(*raiz == NULL){
+        return 1;
+    }
+    return 0;
+}
+
+int alturaArvoreBinaria(ArvoreBinaria* raiz) {
+    if(raiz == NULL || *raiz == NULL){
+        return 0;
+    }
+    int alturaEsq = alturaArvoreBinaria(&((*raiz)->esq));
+    int alturaDir = alturaArvoreBinaria(&((*raiz)->dir));
+    if(alturaEsq > alturaDir){
+        return alturaEsq + 1;
+    } else {
+        return alturaDir + 1;
+    }
+}
+
+int totalNOArvoreBinaria(ArvoreBinaria* raiz) {
+    if(raiz == NULL || *raiz == NULL){
+        return 0;
+    }
+    int totalEsq = totalNOArvoreBinaria(&((*raiz)->esq));
+    int totalDir = totalNOArvoreBinaria(&((*raiz)->dir));
+    return totalEsq + totalDir + 1;
+}
+
 int main() {
     ArvoreBinaria* raiz;
     *raiz = criaArvoreBinaria();
+    liberaArvoreBinaria(raiz);
+    int x = estaVaziaArvoreBinaria(raiz);
+    if(estaVaziaArvoreBinaria(raiz)){
+        printf("A arvore esta vazia!\n");
+    } else {
+        printf("A arvore nao esta vazia!\n");
+    }
+
+    x = alturaArvoreBinaria(raiz);
+    printf("Altura da arvore: %d\n", x);
+    x = totalNOArvoreBinaria(raiz);
+    printf("Total de nos na arvore: %d\n", x);
+
     return 0;
 }
--- binary-tree/programacao-descomplicada/ArvoreBinaria.h
+++ binary-tree/programacao-descomplicada/ArvoreBinaria.h
@@ -1,3 +1,11 @@
 typedef struct NO* ArvoreBinaria;
 
 ArvoreBinaria* criaArvoreBinaria();
+
+void liberaArvoreBinaria(ArvoreBinaria* raiz);
+
+int estaVaziaArvoreBinaria(ArvoreBinaria* raiz);
+
+int alturaArvoreBinaria(ArvoreBinaria* raiz);
+
+int totalNOArvoreBinaria(ArvoreBinaria* raiz);
