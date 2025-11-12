9c84271404d1232e02a452d0e01720dc902ff0f8
marcelloCoder <deoliveiral767@gmail.com>
2025-11-12 17:49:21 -0300

    Rotacao Arvore AVL

--- binary-tree/programacao-descomplicada/ArvoreAVL.c
+++ binary-tree/programacao-descomplicada/ArvoreAVL.c
@@ -20,6 +20,42 @@ int fatorBalanceamentoNO(struct NO* no){
     return labs(alturaNO(no->esq) - alturaNO(no->dir));
 } 
 
+void rotacaoLL(ArvoreAVL *raiz){
+    struct NO *no;
+    no = (*raiz)->esq;
+    (*raiz)->esq = no->dir;
+    no->dir = *raiz;
+    (*raiz)->altura = maior(alturaNO((*raiz)->esq),
+                            alturaNO((*raiz)->dir))
+                            + 1;
+    no->altura = maior(alturaNO(no->esq),
+                        (*raiz)->altura) + 1;
+    *raiz = no;
+}
+
+void rotacaoRR(ArvoreAVL *raiz){
+    struct NO *no;
+    no = (*raiz)->dir;
+    (*raiz)->dir = no->esq;
+    no->esq = *raiz;
+    (*raiz)->altura = maior(alturaNO((*raiz)->esq),
+                            alturaNO((*raiz)->dir))
+                            + 1;
+    no->altura = maior(alturaNO(no->dir),
+                        (*raiz)->altura) + 1;
+    *raiz = no;
+}
+
+void rotacaoLR(ArvoreAVL *raiz){
+    rotacaoRR(&(*raiz)->esq);
+    rotacaoLL(raiz);
+}
+
+void rotacaoRL(ArvoreAVL *raiz){
+    rotacaoLL(&(*raiz)->dir);
+    rotacaoRR(raiz);
+}
+
 int maior(int x, int y){
     if(x > y){
         return x;
